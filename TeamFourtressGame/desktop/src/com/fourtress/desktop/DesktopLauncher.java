889abd49a4640e1cbb1233129e18a7f701d48563
Jenkins <azi.jenkins@capgemini.com>
2018-09-24 11:26:11 +0100

    fresh install of libgdx

---
+++ TeamFourtressGame/desktop/src/com/fourtress/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.fourtress.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.fourtress.TeamFourtressGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TeamFourtressGame(), config);
+	}
+}
