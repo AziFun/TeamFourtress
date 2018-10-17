9e49c9572b51fb3c040b76befe1dd0a6a7782ad6
Kieran White (Capgemini) <kieran.white@hmrcaspire.com>
2018-10-17 20:26:58 +0100

    Initial lock interface work and combination and padlock implementations

--- TeamFourtressGame/core/src/com/fourtress/model/CombinationLock.java
+++ TeamFourtressGame/core/src/com/fourtress/model/CombinationLock.java
@@ -1,7 +1,51 @@
 package com.fourtress.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import com.fourtress.model.interfaces.Lock;
 
-public class CombinationLock implements Lock{
+public class CombinationLock implements Lock {
+
+	private char[] combinationCode;
+
+	public CombinationLock(char[] combinationCode) {
+		this.combinationCode = combinationCode;
+		lock();
+
+	}
+
+	public void tryLock() {
+		Scanner comboScanner = new Scanner(System.in);
+		System.out.println("Please enter the combination code which you want to try: ");
+		String enteredCombo = comboScanner.nextLine();
+		tryCombination(enteredCombo);
+		comboScanner.close();
+	}
+
+	private void tryCombination(String codeAttempt) {
+		char[] codeToTry = codeAttempt.toCharArray();
+		if (combinationCode.equals(codeToTry)) {
+			System.out.println("Combination was correct! The lock is unlocked ");
+			unlock();
+		} else if (codeToTry.length != codeAttempt.length()) {
+			System.out.println("Combination was incorrect! Your code had " + codeAttempt.length()
+					+ "digits and we expect: " + combinationCode.length);
+		} else {
+			List<String> checks = new ArrayList<>();
+			for (int i = 0; i < codeToTry.length; i++) {
+				Character attempt = codeToTry[i];
+				Character actual = combinationCode[i];
+				if (attempt == actual) {
+					checks.add(i + ": Correct!");
+				} else {
+					checks.add(i + ": Incorrect!");
+				}
+			}
+			System.out.println("Combination was incorrect!" + checks.toString());
+		}
+
+	}
 
 }
--- TeamFourtressGame/core/src/com/fourtress/model/interfaces/Lock.java
+++ TeamFourtressGame/core/src/com/fourtress/model/interfaces/Lock.java
@@ -1,5 +1,21 @@
 package com.fourtress.model.interfaces;
 
 public interface Lock {
+	
+	private boolean locked;
+	
+	
+	public default boolean isLocked() {
+		return locked;
+	}
+	
+	default void lock() {
+		this.locked = false;
+	}
+	
+	default void unlock() {
+		this.locked = true;
+	}
+	
 
 }
