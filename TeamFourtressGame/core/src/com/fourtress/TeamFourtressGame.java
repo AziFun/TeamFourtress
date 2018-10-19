402f7f0ffc93cf5f8820871c59237b53614f3d8f
Bilton <rosie.bilton@capgemini.com>
2018-10-19 18:46:52 +0100

    Room and Player class to display

--- TeamFourtressGame/core/src/com/fourtress/TeamFourtressGame.java
+++ TeamFourtressGame/core/src/com/fourtress/TeamFourtressGame.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.fourtress.model.Room;
 import com.fourtress.views.LoadingScreen;
 import com.fourtress.views.MenuScreen;
 import com.fourtress.views.PreferencesScreen;
