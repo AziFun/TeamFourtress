6360b53799a318961bffa0cd3fd978702e8a9102
Kieran White (Capgemini) <kieran.white@hmrcaspire.com>
2018-11-10 14:12:43 +0000

    popup box initial work with default skin added

--- TeamFourtressGame/core/src/com/fourtress/views/GameScreen.java
+++ TeamFourtressGame/core/src/com/fourtress/views/GameScreen.java
@@ -9,10 +9,16 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.fourtress.TeamFourtressGame;
 import com.fourtress.controller.KeyboardController;
 import com.fourtress.model.Box2dModel;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+
 
 public class GameScreen implements Screen{
 	
@@ -22,16 +28,21 @@ public class GameScreen implements Screen{
 	public TeamFourtressGame parent;
 	private KeyboardController controller;
 	private SpriteBatch sb;
+	private Stage stage;
+	private Skin skin;
 	
 	
 	public GameScreen(TeamFourtressGame parent) {
 		this.parent = parent;
 		cam = new OrthographicCamera(32,24);
+		stage = new Stage(new ScreenViewport());
 		controller = new KeyboardController();
+		skin = new Skin(Gdx.files.internal("assets/default/skin/uiskin.json"));
 		model = new Box2dModel(cam, controller, this);
 		debugRenderer = new Box2DDebugRenderer(true,true,true,true,true,true);
 		sb = new SpriteBatch();
 		sb.setProjectionMatrix(cam.combined);
+		
 	}
 
 	@Override
@@ -50,13 +61,20 @@ public class GameScreen implements Screen{
 		Texture background = new Texture(Gdx.files.internal("background.png"));
 		sb.draw(background,-10,-10,20,20);
 		sb.draw(playerSprite,model.player.getPosition().x-1,model.player.getPosition().y-1,2,2);
+		Dialog yourmsgbox = new Dialog("Test popup Box", skin);
+		yourmsgbox.setPosition(50, 10);
+		yourmsgbox.show(stage);
 
 		sb.end();
+		stage.draw();
+
 	}
 
 	@Override
 	public void resize(int width, int height) {
 		// TODO Auto-generated method stub
+		stage.getViewport().update(width, height, true);		
+
 		
 	}
 
