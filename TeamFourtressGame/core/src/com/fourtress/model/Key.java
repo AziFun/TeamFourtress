81615d41f803e72e5806b219d2dbd688a5f5a569
Rosie <biltonr:aston.ac.uk>
2018-11-11 14:01:08 +0000

    New Inventory and Item class with minor amendments to the key class to extend Item superclass

--- TeamFourtressGame/core/src/com/fourtress/model/Key.java
+++ TeamFourtressGame/core/src/com/fourtress/model/Key.java
@@ -1,16 +1,26 @@
 package com.fourtress.model;
 
-public class Key {
-	
+public class Key extends Item {
 	
 	private String keyColour;
+	
+	public Key(String name, String type, String imageName, String keyColour) {
+		super(name, type, imageName);
+		setKeyColor(keyColour);
+	}
+	
 	
 	public String getKeyColor() {
 		return keyColour;
 	}
 
-	
 	private void setKeyColor(String keyColour) {
 		this.keyColour=keyColour;
 	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + " | Key Colour is: " + keyColour;
+	}
+	
 }
